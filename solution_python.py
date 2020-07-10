2b37cacf0261a6168c98e59ea95e8cc74441d520
Abhishek Chakraborty <abhishek.chakraborty@mail.utoronto.ca>
2020-07-10 14:45:00 -0400

    Abhi: Committing my solution for challenge implemented with Stack

--- solution_python.py
+++ solution_python.py
@@ -3,21 +3,53 @@ class EventSourcer():
 
     def __init__(self):
         self.value = 0
+        self.undo_stack = Stack()
+        self.redo_stack = Stack()
 
     def add(self, num: int):
-        pass
+        self.undo_stack.push(-1 * num)
+        self.value = self.value + num
 
     def subtract(self, num: int):
-        pass
+        self.undo_stack.push(num)
+        self.value = self.value - num
 
     def undo(self):
-        pass
+        if self.undo_stack.isEmpty() is False:
+            undo_val = self.undo_stack.pop()
+            self.value = self.value + undo_val
+            self.redo_stack.push(-1 * undo_val)
 
     def redo(self):
-        pass
+        if self.redo_stack.isEmpty() is False:
+            redo_val = self.redo_stack.pop()
+            self.value = self.value + redo_val
+            self.undo_stack.push(-1 * redo_val)
 
     def bulk_undo(self, steps: int):
-        pass
+        for _ in range(steps):
+            if self.undo_stack.isEmpty():
+                break
+            else:
+                self.undo()
 
     def bulk_redo(self, steps: int):
-        pass
+        for _ in range(steps):
+            if self.redo_stack.isEmpty():
+                break
+            else:
+                self.redo()
+
+class Stack():
+    def __init__(self):
+        self.items = []
+    
+    def isEmpty(self):
+        return self.items == []
+    
+    def push(self, val : int):
+        self.items.append(val)
+    
+    def pop(self):
+        if self.isEmpty() is False:
+            return self.items.pop()
